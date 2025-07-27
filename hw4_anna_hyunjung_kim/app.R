e730b43e0753e7c63f6e3738a71aaae0dee941fd
Anna Hyunjung Kim <annakim9237@gmail.com>
2025-07-27 10:59:39 -0400

    scatter plot test

--- hw4_anna_hyunjung_kim/app.R
+++ hw4_anna_hyunjung_kim/app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(ggplot2)
 #remove.packages("DT")
-#install.packages("DT", dependencies = FALSE)
+#install.packages("DT", dependencies = FALSE) #solving error
 library(DT)
 
 ui <- fluidPage(
@@ -12,14 +12,17 @@ ui <- fluidPage(
       checkboxGroupInput("columns", "Select variance you want to see", 
                          choices = names(mtcars),
                          selected = c("mpg", "cyl", "hp")),
-      selectInput("select1", "Choose the variance", choices = names(mtcars))
+      selectInput("select1", "Choose the variance for Boxplot", choices = names(mtcars))
     ),
     
     mainPanel(
       DT::dataTableOutput("data"),
+      tabPanel("Summary", verbatimTextOutput("summary")),
       tabsetPanel(
-        tabPanel("Test Boxplot", plotOutput("Boxplot"))
+        tabPanel("Test Boxplot", plotOutput("Boxplot")),
+        tabPanel("Scatter Plot", plotOutput("plot", width = "60%", height ="400px"))
       )
+      
     )
   )
 
@@ -29,11 +32,24 @@ server <- function(input, output, session) {
   output$data <- DT::renderDataTable({
     mtcars[, input$columns, drop = FALSE]
   })
+  
+  output$summary <- renderPrint({
+    summary(mtcars)
+  })
+  
   output$Boxplot <- renderPlot(
     ggplot(mtcars, aes(x = factor(cyl), y = mtcars[[input$select1]])) +
       geom_boxplot(fill = "lightblue") +
       labs(x = "cyl", y = "mpg", title = "mpg x cyp Bpxplot")
   )
+  
+  output$plot <- renderPlot({
+    ggplot(mpg, aes(x = displ, y = hwy)) +
+      geom_point() +
+      theme_bw() +
+      xlab("Displacement") +
+      ylab("Highway MPG")
+  })
 }
 
 shinyApp(ui, server)
