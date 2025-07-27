35c16d93da81de32a9e30024d7566c96c9583831
Anna Hyunjung Kim <annakim9237@gmail.com>
2025-07-27 10:30:10 -0400

    first box plot but data is not changed when select input.

--- hw4_anna_hyunjung_kim/app.R
+++ hw4_anna_hyunjung_kim/app.R
@@ -1,14 +1,31 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
   titlePanel("HW4 Anna Kim"),
-  mainPanel(
-    tableOutput('data')
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("select1", "Choose the variance", choices = names(mtcars))
+    ),
+    
+    mainPanel(
+      tableOutput('data'),
+      tabsetPanel(
+        tabPanel("Test Boxplot", plotOutput("Boxplot"))
+      )
+    )
   )
+
 )
 
 server <- function(input, output, session) {
   output$data <- renderTable(head(mtcars))
+  output$Boxplot <- renderPlot(
+    ggplot(mtcars, aes(x = factor(cyl), y = mpg)) +
+      geom_boxplot(fill = "lightblue") +
+      labs(x = "cyl", y = "mpg", title = "mpg x cyp Bpxplot")
+  )
 }
 
 shinyApp(ui, server)
