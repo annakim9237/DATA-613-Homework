94174ce443eb2396b053f86072075c9215e50eef
Anna Hyunjung Kim <annakim9237@gmail.com>
2025-08-08 23:27:36 -0400

    HW6 start

--- hw4_anna_hyunjung_kim/app.R
+++ hw4_anna_hyunjung_kim/app.R
@@ -5,17 +5,16 @@ library(ggplot2)
 library(DT)
 
 ui <- fluidPage(
-  titlePanel("HW4 Anna Kim"),
+  titlePanel("Summer 2025 Sessions DATA-613_001 HW4 Anna Kim"),
   
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput("columns", "Select variance you want to see", 
-                         choices = names(mtcars),
-                         selected = c("mpg", "cyl", "hp")),
-      selectInput("select1", "Choose the variable for Box plot", choices = names(mtcars)),
-      selectInput("xvar_scatter", "Choose X variable for Scatter Plot", choices = names(mtcars), selected = "wt"),
-      selectInput("yvar_scatter", "Choose Y variable for Scatter Plot", choices = names(mtcars), selected = "mpg"),
-      selectInput("var_hist", "Choose variable for Histogram", choices = names(mtcars), selected = "mpg")
+      checkboxGroupInput("columns", "Select variance you want to see", choices = names(mtcars), selected = c("mpg", "cyl", "hp")),
+      selectInput("var_box", "Choose the variable for Box plot", choices = names(mtcars)), # For Box plot
+      selectInput("xvar_scatter", "Choose X variable for Scatter Plot", choices = names(mtcars), selected = "wt"), # For Scatter plot
+      selectInput("yvar_scatter", "Choose Y variable for Scatter Plot", choices = names(mtcars), selected = "mpg"), # For Scatter plot
+      selectInput("var_hist", "Choose variable for Histogram", choices = names(mtcars), selected = "mpg"), # For histogram
+      selectInput("var_bar", "Choose variable for Bar Plot", choices = c("cyl", "gear", "carb"), selected = "cyl") # For bar plot
     ),
     
     mainPanel(
@@ -24,8 +23,8 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel("Test Boxplot", plotOutput("Boxplot")),
         tabPanel("Scatter Plot", plotOutput("plot", width = "90%")),
-        tabPanel("Histogram", plotOutput("histplot")
-        )
+        tabPanel("Histogram", plotOutput("histplot")),
+        tabPanel("Box Plot", plotOutput('barplot'))
       )
       
     )
@@ -38,16 +37,18 @@ server <- function(input, output, session) {
     mtcars[, input$columns, drop = FALSE]
   })
   
-  output$summary <- renderPrint({
+  output$summary <- renderPrint(
     summary(mtcars)
-  })
+  ) # () is okay in single line but we need {} on multiple
   
-  output$Boxplot <- renderPlot(
-    ggplot(mtcars, aes(x = factor(cyl), y = mtcars[[input$select1]])) +
+  # boxplot server code
+  output$Boxplot <- renderPlot({
+    ggplot(mtcars, aes(x = factor(cyl), y = mtcars[[input$var_box]])) +
       geom_boxplot(fill = "lightblue") +
       labs(x = "cyl", y = "mpg", title = "mpg x cyp Bpxplot")
-  )
+  })
   
+  #scatter plot server code
   output$plot <- renderPlot({
     ggplot(mtcars, aes(x = .data[[input$xvar_scatter]], y = .data[[input$yvar_scatter]])) +
       geom_point() +
@@ -56,10 +57,18 @@ server <- function(input, output, session) {
       ylab("Highway MPG")
   })
   
+  # Histogram server code
   output$histplot <- renderPlot({
     hist(mtcars[[input$var_hist]],
          xlab = input$var_hist,
          col = "pink")
+  })
+  
+  # Box plot server code
+  output$barplot <- renderPlot({
+    ggplot(mtcars, aes(x = factor(.data[[input$var_bar]]))) +
+      geom_bar(fill = "darkgreen") +
+      ggtitle("Bar Plot")
   })
 }
 
